2f8391e5dff6b56c88d6e061bcba99a1220d8ddc
Taylor <taylorhcarroll1@gmail.com>
2020-04-14 11:24:29 -0700

    initial commit

---
+++ Workforce-Purple-Parrots/Workforce-Purple-Parrots/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Workforce_Purple_Parrots.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
