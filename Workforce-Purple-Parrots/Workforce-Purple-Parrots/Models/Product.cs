eccbfaca9f1f9b3878dac4f50727dccab79eda66
Coffey <jcoffeymay6@gmail.com>
2020-04-15 10:33:33 -0600

    added models

---
+++ Workforce-Purple-Parrots/Workforce-Purple-Parrots/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Workforce_Purple_Parrots.Models
+{
+    public class Product
+    {
+    }
+}
---
+++ Workforce-Purple-Parrots/Workforce-Purple-Parrots/Models/UserPaymentType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Workforce_Purple_Parrots.Models
+{
+    public class UserPaymentType
+    {
+        public int Id { get; set; }
+        public string AcctNumber { get; set; }
+        public bool Active { get; set; }
+        public int CustomerId { get; set; }
+        public int PaymentTypeId { get; set; }
+    }
+}
