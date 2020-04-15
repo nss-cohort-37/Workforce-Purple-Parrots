0c04b6af5c7dd9fe3f5075adb452ff6643628ef5
Coffey <jcoffeymay6@gmail.com>
2020-04-15 13:09:08 -0600

    began impleminting employees details view data

--- Workforce-Purple-Parrots/Workforce-Purple-Parrots/Models/Customer.cs
+++ Workforce-Purple-Parrots/Workforce-Purple-Parrots/Models/Customer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Workforce_Silver_Snakes.Models
+namespace Workforce_Purple_Parrots.Models
 {
     public class Customer
     {
