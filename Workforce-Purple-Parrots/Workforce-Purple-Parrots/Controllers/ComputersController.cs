12754fc575bf3727767fd88a531e67429acb170a
jwgilliam <ggsmoustache@gmail.com>
2020-04-16 13:16:34 -0500

    started work on computers controller

---
+++ Workforce-Purple-Parrots/Workforce-Purple-Parrots/Controllers/ComputersController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Workforce_Purple_Parrots.Controllers
+{
+    public class ComputersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
